8b4be9b2becae43f77ba5cbe7462007820229e61
Carl Oscar Aaro <hello@carloscar.com>
2020-01-04 02:21:00 +0100

    Fixes linting issue

--- tests/test_options.py
+++ tests/test_options.py
@@ -35,7 +35,7 @@ def test_options():
         | cure.KEYWORD_TRAILING_UNDERSCORES
         | cure.KEYWORD_TRAILING_UNDERSCORES
         | cure.KEYWORD_CAMEL_CASE
-    ) == [cure.KEYWORD_TRAILING_UNDERSCORES, cure.KEYWORD_SNAKE_CASE_RECURSIVE, cure.KEYWORD_CAMEL_CASE,]
+    ) == [cure.KEYWORD_TRAILING_UNDERSCORES, cure.KEYWORD_SNAKE_CASE_RECURSIVE, cure.KEYWORD_CAMEL_CASE]
 
     with pytest.raises(TypeError):
         cure.get_options(0)
