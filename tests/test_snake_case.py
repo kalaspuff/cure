493038e46d3b0776d9cbd84cb34144c8c6a79877
Carl Oscar Aaro <hello@carloscar.com>
2020-01-04 02:04:51 +0100

    Black

--- tests/test_snake_case.py
+++ tests/test_snake_case.py
@@ -77,13 +77,33 @@ def values(**kwargs):
         ),
         (
             KEYWORD_SNAKE_CASE_RECURSIVE,
-            {"resources": [{"resourceId": "1", "data": "ABC", "other-value": True}, {"resourceId": "2", "data": "DEF", "other-value": True}]},
-            {"resources": [{"resource_id": "1", "data": "ABC", "other_value": True}, {"resource_id": "2", "data": "DEF", "other_value": True}]},
+            {
+                "resources": [
+                    {"resourceId": "1", "data": "ABC", "other-value": True},
+                    {"resourceId": "2", "data": "DEF", "other-value": True},
+                ]
+            },
+            {
+                "resources": [
+                    {"resource_id": "1", "data": "ABC", "other_value": True},
+                    {"resource_id": "2", "data": "DEF", "other_value": True},
+                ]
+            },
         ),
         (
             KEYWORD_SNAKE_CASE,
-            {"resources": [{"resourceId": "1", "data": "ABC", "other-value": True}, {"resourceId": "2", "data": "DEF", "other-value": True}]},
-            {"resources": [{"resourceId": "1", "data": "ABC", "other-value": True}, {"resourceId": "2", "data": "DEF", "other-value": True}]},
+            {
+                "resources": [
+                    {"resourceId": "1", "data": "ABC", "other-value": True},
+                    {"resourceId": "2", "data": "DEF", "other-value": True},
+                ]
+            },
+            {
+                "resources": [
+                    {"resourceId": "1", "data": "ABC", "other-value": True},
+                    {"resourceId": "2", "data": "DEF", "other-value": True},
+                ]
+            },
         ),
     ],
 )
