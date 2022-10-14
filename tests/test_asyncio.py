785091960e5ea1270ca73a3dd40e233062cb9687
Carl Oscar Aaro <hello@carloscar.com>
2022-10-14 14:00:05 +0200

    Fixes deprecation warning in Python 3.10

--- tests/test_asyncio.py
+++ tests/test_asyncio.py
@@ -1,4 +1,5 @@
 import asyncio
+import sys
 
 import cure
 
@@ -48,7 +49,15 @@ def test_trailing_underscores_decorated_functions():
 
     assert not completed
 
-    loop = asyncio.get_event_loop()
+    if sys.version_info.major == 3 and sys.version_info.minor < 10:
+        loop = asyncio.get_event_loop()
+    else:
+        try:
+            loop = asyncio.get_running_loop()
+        except RuntimeError:
+            loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+
     loop.run_until_complete(_async())
 
     assert completed
